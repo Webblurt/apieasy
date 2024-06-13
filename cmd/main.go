b67d095efda3faf3ee58ab96cc61c0f2a34107b3
vitovt <vitovt@practicalse.com>
2024-06-13 18:17:30 +0300

    uploading the project

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/webblurt/apieasy"
+	"github.com/Webblurt/apieasy"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/webblurt/apieasy
+module github.com/Webblurt/apieasy
 
 go 1.22.3
